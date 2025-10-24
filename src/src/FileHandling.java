ab60e1dad295ae0ca9b261d18b8baa4b61fdc3ed
sketlafox <es.ahmed.bakr2023@alexu.edu.eg>
2025-10-24 16:25:31 +0300

    .

--- src/src/FileHandling.java
+++ src/src/FileHandling.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -9,33 +7,58 @@ public class FileHandling {
 
         public static ArrayList<Student> loadFromFile () {
             try {
-        Path filePath = Paths.get(System.getProperty("user.dir"), "Students.txt");
-        BufferedReader r = new BufferedReader(new FileReader(filePath.toFile()));
-        ArrayList<Student> students = new ArrayList<>();
-        String line;
-        Student e;
-        while ((line = r.readLine()) != null) {
-            e = createRecordFrom(line);
-            students.add(e);
-            return students;
-        }
+                Path filePath = Paths.get(System.getProperty("user.dir"), "Students.txt");
+                BufferedReader r = new BufferedReader(new FileReader(filePath.toFile()));
+                ArrayList<Student> students = new ArrayList<>();
+                String line;
+                Student e;
+                while ((line = r.readLine()) != null) {
+                    e = createRecordFrom(line);
+                    students.add(e);
+                    return students;
+                }
 
-    }catch (IOException e){
+            } catch (IOException e) {
                 System.out.println("this file cannot be opened!");
-                return;
-            }
 
-    }
+
+            }
+            return null;
+
+        }
     public static Student createRecordFrom(String line) {
         Student e;
         String[] data = line.split(",");
         String StudentId=data[0];
         String Name = data[1];
-        String Email = data[2];
-        String Address = data[3];
-        String PhoneNumber = data[4];
-        e = new EmployeeUser(employeeId,Name,Email,Address,PhoneNumber);
+        int Age = Integer.parseInt(data[2]);
+        String Gender = data[3];
+        String Department = data[4];
+        float GPA = Float.parseFloat(data[5]);
+        String Password = data[6];
+        e = new Student( StudentId, Name,  Age, Gender, Department,  GPA,Password);
         return e;
+    }
+
+    public void saveToFile(ArrayList<Student> students)
+    {
+        try {
+            Path filePath = Paths.get(System.getProperty("user.dir"), "Students.txt");
+            BufferedWriter w = new BufferedWriter(new FileWriter(filePath.toFile()));
+            String line;
+            for(Student rec :students )
+            {
+                line = rec.lineRepresentation();
+                w.write(line);
+                w.newLine();
+            }
+            w.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("There is an error while writing in file!");
+            return;
+        }
     }
 
 }
